37a6bf954dfcfa98b0c4018f8c42cb5a657ead1c
pitikorn32 <pitikorn32@gmail.com>
2022-12-29 13:00:14 +0700

    feat: add email

--- src/components/section-about/index.jsx
+++ src/components/section-about/index.jsx
@@ -7,7 +7,8 @@ const SectionAbout = ({ about }) => {
     <Section title="About Me">
       <div className="mb-6">
         <p>{about}</p>
-        <p>email: pitikorn32@gmail.com</p>
+        <p><b>email:</b> pitikorn32@gmail.com</p>
+        <p><b>tel:</b> 092-282-0916</p>
       </div>
     </Section>
   );
