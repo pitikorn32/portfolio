759cb2458b8b312a9a862e89a5542d63a48056e2
pitikorn32 <pitikorn32@gmail.com>
2023-06-04 12:36:06 +0700

    feat: part1

--- portfolio/docusaurus.config.js
+++ portfolio/docusaurus.config.js
@@ -61,11 +61,6 @@ const config = {
           src: 'img/circular-profile-picture.png',
         },
         items: [
-          { 
-            to: "/blog", 
-            label: "Blog", 
-            position: "left" 
-          },
           {
             to: 'my-projects/project-viznet',
             position: 'left',
@@ -110,47 +105,6 @@ const config = {
       },
       footer: {
         style: 'dark',
-        // links: [
-        //   {
-        //     title: 'Docs',
-        //     items: [
-        //       {
-        //         label: 'Tutorial',
-        //         to: '/docs/intro',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'Community',
-        //     items: [
-        //       {
-        //         label: 'Stack Overflow',
-        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //       },
-        //       {
-        //         label: 'Discord',
-        //         href: 'https://discordapp.com/invite/docusaurus',
-        //       },
-        //       {
-        //         label: 'Twitter',
-        //         href: 'https://twitter.com/docusaurus',
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     title: 'More',
-        //     items: [
-        //       {
-        //         label: 'Blog',
-        //         to: '/blog',
-        //       },
-        //       {
-        //         label: 'GitHub',
-        //         href: 'https://github.com/facebook/docusaurus',
-        //       },
-        //     ],
-        //   },
-        // ],
         copyright: `Copyright © ${new Date().getFullYear()} Logan Zehm`,
       },
       prism: {
